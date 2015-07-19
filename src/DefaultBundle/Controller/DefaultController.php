a927a8c15b6585ad636376836787fcdbdacadd56
Vincent Langlet <vincentl@theodo.fr>
2015-07-19 23:36:25 +0200

    Populate Navbar

--- src/DefaultBundle/Controller/DefaultController.php
+++ src/DefaultBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace DefaultBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -17,7 +16,51 @@ class DefaultController extends Controller
      *
      * @return array
      */
-    public function indexAction()
+    public function homeAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/presentation", name="presentation")
+     * @Template
+     *
+     * @return array
+     */
+    public function presentationAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/schedule", name="schedule")
+     * @Template
+     *
+     * @return array
+     */
+    public function scheduleAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/prizes", name="prizes")
+     * @Template
+     *
+     * @return array
+     */
+    public function prizesAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/partners", name="partners")
+     * @Template
+     *
+     * @return array
+     */
+    public function partnersAction()
     {
         return array();
     }
--- src/DefaultBundle/Tests/Controller/DefaultControllerTest.php
+++ src/DefaultBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,13 +6,57 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testHome()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+    }
+
+    public function testPresentation()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/presentation');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testSchedule()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/schedule');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testPrizes()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/prizes');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testParners()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/partners');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testContact()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/contact');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
